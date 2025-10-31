32701948bfbaab9c8a74d92c854da3d98894f914
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-10-31 17:59:35 +0330

    creating main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+
+async function main(){
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static('public'));
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+
+main();
